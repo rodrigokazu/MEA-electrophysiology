e20368e17d7461ce07081a2434bf2b94d8812e8f
Rodrigo <rodrigokazu68@gmail.com>
2019-02-18 20:19:50 +0000

    Changed name of the MATLAB folder to MATLAB scripts for MEA analysis

--- MATLAB/meatools/mcintfac/plotmea.m
+++
@@ -1,5 +0,0 @@
-function plotmea(d)
-% plotmea- plot the channels in this recording (datastream method)
-% plotmea(d) plots the channels in an MCRack file
-
-	cellplot(reshape(d.ChannelNames([1:64]),8,8));
--- MATLAB/meatools/mcintfac/setid.m
+++
@@ -1,6 +0,0 @@
-function a=setid(d,id)
-% setid - not used, retained for compatibility
-%set a.id
-
-a=d;
-a.id=id;
